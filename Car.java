d44d1f4e722635a6c587bea14ee4b32d5aac97c3
dulinawork <nimadithd7@gmail.com>
2026-06-16 14:56:11 +0530

    New file added to commit

---
+++ Car.java
@@ -0,0 +1,48 @@
+public class Car {
+    
+    // Private fields
+    private String brand;
+    private int age;
+    private String numberPlate;
+
+    // Constructor
+    public Car(String brand, int age, String numberPlate) {
+        this.brand = brand;
+        this.age = age;
+        this.numberPlate = numberPlate;
+    }
+
+    // Accessors (Getters)
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    // Mutators (Setters)
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Car [brand=" + brand +
+               ", age=" + age +
+               ", numberPlate=" + numberPlate + "]";
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        // Create two Car objects
+        Car car1 = new Car("Toyota", 5, "ABC-1234");
+        Car car2 = new Car("Honda", 3, "XYZ-5678");
+
+        // Call toString() for each object
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+    }
+}
